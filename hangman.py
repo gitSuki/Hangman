df80efe805e67aa1923ec66fb532839538cdbb9b
gitSuki <collinbrown226@gmail.com>
2022-01-22 23:14:27 -0600

    added game class

--- hangman.py
+++ hangman.py
@@ -8,6 +8,22 @@ with open('word_list.txt') as wordlist_file:
         if len(word) > 4 and len(word) < 13:
             filtered_list.append(word)
 
-def random_word(arg_list):
-    word_index = random.randint(0, len(arg_list))
-    return arg_list[word_index]
+
+class Game:
+    def __init__(self):
+        self.welcome()
+        self.word = self.random_word(filtered_list)
+        print(f"Your random word has been chosen, it has {len(self.word)} letters")
+    
+    def welcome(self):
+        print("Welcome to Hangman!")
+        print("")
+        print("Written by gitSuki in in Python 3.8.10")
+        print("")
+        print("")
+
+    def random_word(self, arg_list):
+        word_index = random.randint(0, len(arg_list))
+        return arg_list[word_index]
+
+Game()
