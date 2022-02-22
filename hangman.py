e4effb0f6439e3bfea4be19036a153fabd49de56
gitSuki <collinbrown226@gmail.com>
2022-02-22 16:55:01 -0600

    refactored incorrect guess code and console display

--- hangman.py
+++ hangman.py
@@ -14,35 +14,42 @@ with open('word_list.txt') as wordlist_file:
         if len(word) > 4 and len(word) < 13:
             filtered_list.append(word)
 
+
 class Game:
     def __init__(self):
         self.welcome()
-        self.victory = False
-        self.turn_count = 0                                     #Turn_count counts the amount of times the player has guessed
-        self.incorrect_guesses = 0                              #Counts the total amount of incorrect guesses
-        self.guessed_list = []                                  #A list of all the characters the played has guessed
+        self.victory = False                                    #Determines if the user has guessed the correct word
         self.word = self.random_word(filtered_list)             #Calls the random_word method to select a word from the filtered list
         self.letter_list = list(self.word)                      #Splits the selected word into a list which contains each letter of the word
-        self.incorrect_guess_limit = len(self.word)             #Sets the maximum amount of guesses the user has to correctly guess each letter
-        self.underscore_word = ["_"] * len(self.letter_list)    #Creates a list of underscores to be filled in as the player correctly guesses letters
+        self.underscore_word = ["_"] * len(self.letter_list)    #List of underscores to be filled in as the player correctly guesses letters
+
+        self.turn_count = 0                                     #Counts the amount of times the player has guessed
+        self.guessed_list = []                                  #A list of all letters the player has guessed
+        self.incorrect_guess_list = []                          #A list of all incorrect letters the player has guessed
+        self.incorrect_guesses = 0                              #Counts the total amount of incorrect guesses
+        self.incorrect_guess_limit = len(self.word)             #Sets the maximum amount of guesses the user has
 
         print(f"Your random word has been chosen, it has {len(self.word)} letters")
         print("")
+        print("")
         self.turn()
-    
+
+
     def welcome(self):
         print("Welcome to Hangman!")
         print("")
-        print("Written by gitSuki in in Python 3.8.10")
+        print("A random word of 5-12 words will be chosen. On each turn you can guess one letter from the word. To win you must correctly guess every letter before running out of turns!")
         print("")
-        print("")
+
 
     def random_word(self, arg_list):
         #Generates a random integer from 0 to the length of the list given in the parameter
         word_index = random.randint(0, len(arg_list))
         return arg_list[word_index]
     
+
     def guess_letter(self):
+        #Function to allow the user to input a letter to guess if it's within the randomly selected word
         while True:
             user_input = input()                                #Records the user's input
             user_input = user_input.lower()                     #Converts the user input to lower case to avoid case sensitivity
@@ -60,15 +67,18 @@ class Game:
             else:
                 print("Invalid input. Please input a single letter.")
 
+
     def compare_letter(self, letter):
         if letter in self.letter_list:
             self.replace_underscore(letter)
+
         else:
+            self.incorrect_guess_list.append(letter)
             self.incorrect_guesses += 1
             print("")
             print(f"Incorrect guess: {letter} is not a letter in the selected word")
-
             self.print_progress()
+
 
     def replace_underscore(self, letter):
         index_list = [i for i, index in enumerate(self.letter_list) if index == letter]
@@ -77,17 +87,20 @@ class Game:
 
         self.print_progress()
 
+
     def print_progress(self):
         print("")
         print("")
-        print("Guessed letters so far: " + " ".join(self.guessed_list))
+        print("Incorrect letters gussed so far: " + " ".join(self.incorrect_guess_list))
         print(' '.join(self.underscore_word))
         print("")
+
 
     def check_victory(self):
         if self.letter_list == self.underscore_word:
             self.victory = True
-    
+
+
     def turn(self):
         while self.incorrect_guesses < self.incorrect_guess_limit and not self.victory:
             self.turn_count += 1
@@ -102,8 +115,6 @@ class Game:
             print("Congratulations! You won!")
         else:
             print("")
-            print("You lost!")
-            print("")
-            print("The word was " + self.word)
+            print("The word you were trying to guess was: " + self.word.under())
 
 Game()
